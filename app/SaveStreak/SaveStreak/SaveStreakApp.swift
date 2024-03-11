49c391a91f49502a0b78c13b09c11ef40c898386
Chinmay Yadav <chinmayyadav98@gmail.com>
2024-03-11 11:03:52 -0400

    SwiftUI Code Structure Added

---
+++ app/SaveStreak/SaveStreak/SaveStreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveStreakApp.swift
+//  SaveStreak
+//
+//  Created by Chinmay Yadav on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SaveStreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
