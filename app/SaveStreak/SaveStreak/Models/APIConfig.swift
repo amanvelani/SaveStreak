90bdec46497a01c6eea673e9657e86019119640f
Chinmay Yadav <chinmayyadav98@gmail.com>
2024-04-30 12:24:35 -0400

    UI Changes for the plaid integration in the app

---
+++ app/SaveStreak/SaveStreak/Models/APIConfig.swift
@@ -0,0 +1,23 @@
+//
+//  APIResponse.swift
+//  SaveStreak
+//
+//  Created by Chinmay Yadav on 4/29/24.
+//
+
+import Foundation
+
+class ApiConfig: ObservableObject {
+	@Published var baseUrl = "http://save-streak.live"
+}
+
+
+struct ApiResponse: Codable {
+	var status: String
+	var error: String?
+	
+	enum CodingKeys: String, CodingKey {
+		case status = "Status"
+		case error = "Error"
+	}
+}
